45502b0949920925955457011286739d333fa6cc
TanzBis <tanzbis81@gmail.com>
2021-12-08 10:34:55 +0300

    create: only 28 task

--- js/main.js
+++ js/main.js
@@ -1,22 +1,51 @@
-const hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-});
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  });
+  
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    loop: true,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  });
+  
+  var menuButton = document.querySelector(".menu-button")
+  menuButton.addEventListener('click', function() {
+    console.log("Клик по кнопке меню");
+    document.querySelector(".navbar-bottom")
+    .classList.toggle("navbar-bottom--visible");
+  });
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on('click', openModal);
+  closeModalButton.on('click', closeModal)
+  
+  function openModal() {
+    var modalOverlay = $(".modal__overlay")
+    var modalDialog = $(".modal__dialog")
+    modalOverlay.addClass("modal__overlay--visible")
+    modalDialog.addClass("modal__dialog--visible")
+  }
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  loop: true,
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay")
+    var modalDialog = $(".modal__dialog")
+    modalOverlay.removeClass("modal__overlay--visible")
+    modalDialog.removeClass("modal__dialog--visible")
+  }
+
 });
